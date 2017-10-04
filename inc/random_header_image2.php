3bfa9a65426d62f203d766d7fd632258cc39e4c6
Karin Dalziel <kdalziel2@unl.edu>
2017-10-04 16:15:35 +0000

    small changes for launch

--- inc/random_header_image2.php
+++ inc/random_header_image2.php
@@ -3,9 +3,9 @@
 	
 ?>
 
-<div id="home_banner">
+ <div id="home_banner">
     <div id="home_banner_bg">
-    <div id="home_banner_photo<?= $ranNum ?>" class="home_banner_photo">
+    <div id="home_banner_photo<?php  echo $ranNum  ?>" class="home_banner_photo">
     </div>
     <div id="home_banner_tagline">The Journal for Ancient Performance</div>
     </div>
